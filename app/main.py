30c95bea3fbb9e4e372c0088cab6a029217b9fca
Pratik Saha <p.saha@reply.de>
2024-04-03 17:11:58 +0200

    add login page as homepage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
-from .routes.users import router as UserRouter
-from .config import DATABASE_URL, test_connection
+from fastapi.responses import FileResponse
+from app.routes.users import router as UserRouter
+from app.config import DATABASE_URL, test_connection
+from app.models.models import Base, engine  # Import Base and engine from your models module
 
 app = FastAPI()
 
 async def startup_event():
     await test_connection()
 
+    # Create the database tables
+    Base.metadata.create_all(bind=engine)
+
 app.add_event_handler("startup", startup_event)
 
 app.include_router(UserRouter)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
+@app.get("/", response_class=FileResponse)
+async def read_home():
+    return "./app/static/UI/user.html"
