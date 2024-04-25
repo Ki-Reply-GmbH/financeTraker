42fae51ab1ad893b3b740b62342054d48f2c5430
Hummel, Daniel (PPN) <d.hummel@reply.de>
2024-04-25 13:47:50 +0200

    fix: pythonsecurity:S2083

--- app/main.py
+++ app/main.py
@@ -1,53 +1,40 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 from routes.users import router as UserRouter
 from routes.transactions import router as TransactionRouter
 from config import DATABASE_URL, test_connection, secret_generator
-from models.models import Base, engine  # Import Base and engine from your models module
+from models.models import Base, engine
 from logger import get_logger
 import os
-from fastapi import HTTPException
 
 logger = get_logger(__name__)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="./app/static"), name="static")
 
 async def test_database_connection_on_startup():
-
     await test_connection()
-    # Create the database tables
     Base.metadata.create_all(bind=engine)
 
 app.include_router(UserRouter)
 app.include_router(TransactionRouter)
-
 app.add_event_handler("startup", test_database_connection_on_startup)
 
-# if in the env file SECREAT_KEY is not set, generate a new one
 if not os.getenv("SECRET_KEY"):
     logger.info("Generating new secret key")
     secret=secret_generator()
     with open('.env', 'a') as f:
         f.write(f'\nSECRET_KEY={secret}\n')
     
-
 @app.get("/", response_class=HTMLResponse)
 async def read_home():
-    path= os.path.abspath("./app/static/UI/dashboard.html")
-    if not os.path.exists(path):
-        raise HTTPException(status_code=404, detail="Page not found")
-    with open(path) as f:
-        html_content = f.read()
-    return html_content
+    return FileResponse("./app/static/UI/dashboard.html")
 
 @app.get("/ui/{id}", response_class=HTMLResponse)
 async def read_ui(id: str):
-    path = os.path.abspath(f"./app/static/UI/{id}.html")
-    if not os.path.exists(path):
-        raise HTTPException(status_code=404, detail="Page not found")
-    with open(path) as f:
-        html_content = f.read()
-    return html_content
+    return FileResponse(path=f"./app/static/UI/{id}.html", headers={"Content-Disposition": "inline; filename='{id}.html'"})
 
 if __name__ == "__main__":
     import uvicorn
